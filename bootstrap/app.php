dffaeec7b600064ea9e5345862fa647ff23a6677
Nick <seramity@outlook.com>
2017-05-26 16:11:45 -0600

    Database collation fix

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -28,8 +28,8 @@ $app = new \Slim\App([
             'database' => $_ENV['DB_DATABASE'],
             'username' => $_ENV['DB_USERNAME'],
             'password' => $_ENV['DB_PASSWORD'],
-            'charset' => 'utf8',
-            'collation' => 'utf8_unicode_ci',
+            'charset' => 'utf8mb4',
+            'collation' => 'utf8mb4_unicode_ci',
             'prefix' => '',
         ]
     ]
