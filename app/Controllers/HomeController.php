dfa915e9b0d100b85f6f99383126a138d39e0df2
Nick <seramity@outlook.com>
2017-05-27 21:24:55 -0600

    Added welcome landing page

--- app/Controllers/HomeController.php
+++ app/Controllers/HomeController.php
@@ -6,11 +6,14 @@ use App\Models\User;
 use App\Models\UserPermission;
 
 
-
 class HomeController extends Controller
 {
     public function index($request, $response)
     {
-        return $this->view->render($response, 'home.twig');
+        if(!$this->auth->user()) {
+            return $this->view->render($response, 'welcome.twig');
+        } else {
+            return $this->view->render($response, 'home.twig');
+        }
     }
 }
